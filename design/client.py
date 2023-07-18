e876c9a39ef3b4c549ba408cd3614f41deed5809
MarcusStill <belg.marcus@gmail.com>
2023-07-19 00:11:06 +0300

    Small changes in the form of the client,

--- design/client.py
+++ design/client.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-
-from PySide6.QtCore import QCoreApplication, QMetaObject, QRect, Qt
-from PySide6.QtWidgets import (QLabel, QLineEdit, QPushButton, QGridLayout,
-QComboBox, QHBoxLayout, QWidget, QToolBox, QFormLayout, QDateEdit)
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
+                            Qt)
+from PySide6.QtWidgets import (QComboBox, QDateEdit, QFormLayout, QGridLayout, QHBoxLayout, QLabel,
+                               QLineEdit, QPushButton, QToolBox, QWidget)
 
 class Ui_Dialog_Client(object):
     def setupUi(self, Dialog_Client):
         if not Dialog_Client.objectName():
             Dialog_Client.setObjectName(u"Dialog_Client")
         Dialog_Client.setWindowModality(Qt.ApplicationModal)
-        Dialog_Client.resize(346, 313)
+        Dialog_Client.resize(346, 266)
         self.toolBox = QToolBox(Dialog_Client)
         self.toolBox.setObjectName(u"toolBox")
         self.toolBox.setGeometry(QRect(10, 10, 341, 261))
@@ -18,7 +17,7 @@ class Ui_Dialog_Client(object):
         self.client.setGeometry(QRect(0, 0, 341, 231))
         self.layoutWidget = QWidget(self.client)
         self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(0, 0, 336, 221))
+        self.layoutWidget.setGeometry(QRect(0, 0, 333, 181))
         self.formLayout = QFormLayout(self.layoutWidget)
         self.formLayout.setObjectName(u"formLayout")
         self.formLayout.setContentsMargins(0, 0, 0, 0)
@@ -126,7 +125,7 @@ class Ui_Dialog_Client(object):
         self.toolBox.addItem(self.client, u"\u0418\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044f \u043e \u043a\u043b\u0438\u0435\u043d\u0442\u0435")
         self.layoutWidget1 = QWidget(Dialog_Client)
         self.layoutWidget1.setObjectName(u"layoutWidget1")
-        self.layoutWidget1.setGeometry(QRect(80, 280, 183, 30))
+        self.layoutWidget1.setGeometry(QRect(80, 230, 183, 30))
         self.horizontalLayout = QHBoxLayout(self.layoutWidget1)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -140,15 +139,6 @@ class Ui_Dialog_Client(object):
 
         self.horizontalLayout.addWidget(self.pushButton_2)
 
-        QWidget.setTabOrder(self.lineEdit, self.lineEdit_2)
-        QWidget.setTabOrder(self.lineEdit_2, self.lineEdit_3)
-        QWidget.setTabOrder(self.lineEdit_3, self.dateEdit)
-        QWidget.setTabOrder(self.dateEdit, self.comboBox)
-        QWidget.setTabOrder(self.comboBox, self.comboBox_2)
-        QWidget.setTabOrder(self.comboBox_2, self.lineEdit_4)
-        QWidget.setTabOrder(self.lineEdit_4, self.lineEdit_5)
-        QWidget.setTabOrder(self.lineEdit_5, self.pushButton)
-        QWidget.setTabOrder(self.pushButton, self.pushButton_2)
 
         self.retranslateUi(Dialog_Client)
 
@@ -160,13 +150,13 @@ class Ui_Dialog_Client(object):
 
     def retranslateUi(self, Dialog_Client):
         Dialog_Client.setWindowTitle(QCoreApplication.translate("Dialog_Client", u"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u043e", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog_Client", u"\u0418\u043c\u044f", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog_Client", u"\u0418\u043c\u044f *", None))
         self.label_4.setText(QCoreApplication.translate("Dialog_Client", u"\u041f\u043e\u043b", None))
         self.comboBox.setItemText(0, QCoreApplication.translate("Dialog_Client", u"-", None))
         self.comboBox.setItemText(1, QCoreApplication.translate("Dialog_Client", u"\u043c\u0443\u0436", None))
         self.comboBox.setItemText(2, QCoreApplication.translate("Dialog_Client", u"\u0436\u0435\u043d", None))
 
-        self.label.setText(QCoreApplication.translate("Dialog_Client", u"\u0424\u0430\u043c\u0438\u043b\u0438\u044f", None))
+        self.label.setText(QCoreApplication.translate("Dialog_Client", u"\u0424\u0430\u043c\u0438\u043b\u0438\u044f *", None))
         self.label_6.setText(QCoreApplication.translate("Dialog_Client", u"\u0422\u0435\u043b\u0435\u0444\u043e\u043d", None))
         self.label_3.setText(QCoreApplication.translate("Dialog_Client", u"\u0414\u0430\u0442\u0430 \u0440\u043e\u0436\u0434\u0435\u043d\u0438\u044f", None))
         self.label_5.setText(QCoreApplication.translate("Dialog_Client", u"\u041e\u0442\u0447\u0435\u0441\u0442\u0432\u043e", None))
@@ -175,9 +165,10 @@ class Ui_Dialog_Client(object):
         self.comboBox_2.setItemText(1, QCoreApplication.translate("Dialog_Client", u"\u0438", None))
         self.comboBox_2.setItemText(2, QCoreApplication.translate("Dialog_Client", u"\u043c", None))
 
-        self.pushButton_3.setText(QCoreApplication.translate("Dialog_Client", u"\u041a\u043e\u043f\u0438\u0440.", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Dialog_Client", u"\u041a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
         self.pushButton_4.setText(QCoreApplication.translate("Dialog_Client", u"\u0412\u0441\u0442\u0430\u0432\u0438\u0442\u044c", None))
         self.label_7.setText(QCoreApplication.translate("Dialog_Client", u"E-mail", None))
         self.toolBox.setItemText(self.toolBox.indexOf(self.client), QCoreApplication.translate("Dialog_Client", u"\u0418\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044f \u043e \u043a\u043b\u0438\u0435\u043d\u0442\u0435", None))
         self.pushButton.setText(QCoreApplication.translate("Dialog_Client", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c", None))
         self.pushButton_2.setText(QCoreApplication.translate("Dialog_Client", u"\u041e\u0442\u043c\u0435\u043d\u0430", None))
+    # retranslateUi
