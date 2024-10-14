2558ac72cfbe1ab631944c1888507c1b0a592c71
MarcusStill <belg.marcus@gmail.com>
2024-10-14 14:55:03 +0300

    Fixes in the logging function.

--- files/logger.py
+++ files/logger.py
@@ -1,10 +1,14 @@
 import functools
-
 from loguru import logger
 
 
-def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
-
+def logger_wraps(
+    *,
+    entry: bool = True,
+    exit: bool = True,
+    level: str = "DEBUG",
+    catch_exceptions: bool = True
+):
     def wrapper(func):
         name = func.__name__
 
@@ -12,11 +16,18 @@ def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
         def wrapped(*args, **kwargs):
             logger_ = logger.opt(depth=1)
             if entry:
-                logger_.log(level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs)
-            result = func(*args, **kwargs)
-            if exit:
-                logger_.log(level, "Exiting '{}' (result={})", name, result)
-            return result
+                logger_.log(
+                    level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs
+                )
+            try:
+                result = func(*args, **kwargs)
+                if exit:
+                    logger_.log(level, "Exiting '{}' (result={})", name, result)
+                return result
+            except Exception as e:
+                if catch_exceptions:
+                    logger_.error("Exception in '{}': {}", name, str(e))
+                raise  # Повторно выбрасываем исключение, чтобы оно могло быть обработано вызывающим кодом
 
         return wrapped
 
